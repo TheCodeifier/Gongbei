daf99817cb524140ab37f690cae0a63be0291376
Diomuzan <94070451+TheCodeifier@users.noreply.github.com>
2025-05-15 17:47:51 +0200

    Finished with Gongbei

--- src/main.java
+++ src/main.java
@@ -67,7 +67,26 @@ class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Kies een methode om te betalen (iDeal, Creditcard of PayPal):");
+        String choice = scanner.nextLine();
       
-            
+        Payment pay;
+
+        switch (choice) {
+            case "iDeal":
+                pay = new iDeal();
+                break;
+            case "Creditcard":
+                pay = new Creditcard();
+                break;
+            case "PayPal":
+                pay = new PayPal();
+                break;
+            default:
+                System.out.println("Ongeldige keuze");
+                scanner.close();
+                return;
+        } 
+        pay.pay();
+        scanner.close();
     }
 }
